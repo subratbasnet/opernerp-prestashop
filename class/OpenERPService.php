f973b14f4f4f9e459c382921f309a4f9bd3cf0ce
Subrat Basnet <subrat@grepsr.com>
2013-05-19 10:33:15 +0545

    password

--- class/OpenERPService.php
+++ class/OpenERPService.php
@@ -58,13 +58,13 @@ class OpenERPService {
     public function getErpProductIds() {
         $args = array(array("available_in_pos", "=", true));
         $model = 'product.product';
-        return $this->xmlRpcCall('execute', array($this->erpDb, $this->erpUserId, $this->erPwd, $model, 'search', $args));
+        return $this->xmlRpcCall('execute', array($this->erpDb, $this->erpUserId, $this->erpPwd, $model, 'search', $args));
     }
 
     public function getErpProductInfo($id) {
         $args = array('id', 'name', 'lst_price', 'code', 'qty_available', 'price_extra', 'image', 'pos_categ_id', 'active');
         $model = 'product.product';
-        $arr = $this->xmlRpcCall('execute', array($this->erpDb, $this->erpUserId, $this->erPwd, $model, 'read', $id, $args));
+        $arr = $this->xmlRpcCall('execute', array($this->erpDb, $this->erpUserId, $this->erpPwd, $model, 'read', $id, $args));
         if (is_array($arr) && !empty($arr)) {
             $arr['description'] = $this->getErpProductDesc($id);
             $catArr = $arr['pos_categ_id'];
@@ -82,7 +82,7 @@ class OpenERPService {
     public function getErpProductDesc($id) {
         $args = array('description');
         $model = 'product.template';
-        $desc = ($this->xmlRpcCall('execute', array($this->erpDb, $this->erpUserId, $this->erPwd, $model, 'read', $id, $args)));
+        $desc = ($this->xmlRpcCall('execute', array($this->erpDb, $this->erpUserId, $this->erpPwd, $model, 'read', $id, $args)));
         return trim($desc['description']);
     }
 
